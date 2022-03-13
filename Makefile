69dbb4041611c9093cc0f3defe5969d2a8065b79
Zakaria Bidouli <zbidouli@e2r13p9.1337.ma>
2022-03-13 12:13:13 +0100

    minitalk bonus done

--- Makefile
+++ Makefile
@@ -4,11 +4,11 @@ CC 		= gcc
 
 FLAGS	= -Wall -Wextra -Werror
 
-all: s s2 c
+all: c2 s2 c
 
 
-server.o: server.c utils.c minitalk.h
-	$(CC) $(FLAGS) -c server.c utils.c
+# server.o: server.c utils.c minitalk.h
+# 	$(CC) $(FLAGS) -c server.c utils.c
 
 server2.o: server2.c utils.c minitalk.h
 	$(CC) $(FLAGS) -c server2.c utils.c
@@ -19,14 +19,20 @@ utils.o: utils.c minitalk.h
 client.o: client.c utils.c minitalk.h
 	$(CC) $(FLAGS) -c client.c utils.c
 
-s: server.o utils.o
-	$(CC) $(FLAGS)  server.o utils.o -o server
+client2.o: client2.c utils.c minitalk.h
+	$(CC) $(FLAGS) -c client2.c utils.c
+
+# s: server.o utils.o
+# 	$(CC) $(FLAGS)  server.o utils.o -o server
 
 s2: server2.o utils.o
 	$(CC) $(FLAGS)  server2.o utils.o -o server2
 
 c: client.o utils.o 
 	$(CC) $(FLAGS)  client.o utils.o -o client
+
+c2: client2.o utils.o 
+	$(CC) $(FLAGS)  client2.o utils.o -o client2
 	
 clean:
 	rm -rf *.o *~
---
+++ client2.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: zbidouli <zbidouli@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/09 22:12:29 by zbidouli          #+#    #+#             */
+/*   Updated: 2022/03/13 11:28:20 by zbidouli         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+int main(int ac, char  **av)
+{
+	char	*str;
+	int		pid;
+	int		i;
+	int		j;
+
+	if (ac == 3)
+	{
+		pid = ft_atoi(av[1]);
+		str = av[2];
+		i = -1;
+		while(str[++i])
+		{
+			j = 0;
+			while(j < 8)
+			{
+
+				if((str[i]>>j  & 1) == 1) // 000 0110 & 1
+				{
+					if (kill(pid, SIGUSR1) == -1)
+					{
+						ft_putstr_fd("Message reiceved by server ", 1);
+						return(0);
+					}
+				}
+				else 
+				{
+					if(kill(pid, SIGUSR2) == -1)
+						return(0);
+				}
+				j++;
+				usleep(250);
+			}
+		}	
+	}
+	ft_putstr_fd("Message reiceved by server ", 1);
+	exit(0);
+}
+
+//
+	// 5 => 0000 0101;
+	// 1 => 0000 0001;
+
+	// 10
