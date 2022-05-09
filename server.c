f4e2c5a5fd4d7e2cf2d1fb655eba123bcf7169a3
zakaria <bidouli.zak@gmail.com>
2022-05-09 16:50:02 +0100

    mini

--- server.c
+++ server.c
@@ -27,16 +27,12 @@ void	handler(int signum, struct __siginfo *info, void *vo)
 	if(g_char.pid != info->si_pid)
 		reset(info->si_pid);
 	if (signum == SIGUSR1)
-	{
 		g_char.ch ^= 1 << g_char.bit;
-		g_char.bit++;
-	}
-	else if (signum == SIGUSR2)
-			g_char.bit++;
-	if (g_char.ch == 0)
-		kill(SIGUSR1, g_char.pid);
-	else if (g_char.bit == 8)
+	g_char.bit++;
+	if (g_char.bit == 8)
 	{
+	 	if (g_char.ch == 0)
+	 		kill(g_char.pid, SIGUSR1);
 		ft_putchar_fd(g_char.ch, 1);
 		reset(info->si_pid);
 	}
@@ -53,8 +49,6 @@ int	main(void)
 	sa.sa_flags = SA_SIGINFO;
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
-	if (g_char.ch == '\0')
-		kill(g_char.pid, SIGUSR1);
 	while(1)
 		pause();
 	return (0);
